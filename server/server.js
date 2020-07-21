5a7c0208b451ab00d8faf1ab05f6cf26e035b9e4
jaesharma <44082416+jaesharma@users.noreply.github.com>
2020-07-21 09:26:24 +0530

    setup express server & scripts

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+const port=process.env.PORT || 3000;
 const app = express();
 const root = require('path').join(__dirname, '..', 'build')
 app.use(express.static(root));
 app.get('/*', function(req, res) {
   res.sendFile(path.join('index.html',{ root }));
 });
-app.listen(9000);
+app.listen(port,()=>{
+	console.log('server is up');
+});
