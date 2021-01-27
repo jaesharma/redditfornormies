5dffdc501ae9d3452457c2bf3f16b0f6aa4ede5f
Harsh Vardhan <hvkataria12@gmail.com>
2021-01-27 14:55:16 +0530

    Enable CORS & Audit Fix

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const path = require('path');
 const port=process.env.PORT || 3000;
 const app = express();
-const root = require('path').join(__dirname, '..', 'build')
+const root = require('path').join(__dirname, '..', 'build');
+const cors = require('cors');
+
+app.use(cors());
+app.options('*', cors());
 app.use(express.static(root));
 app.get('*', function(req, res) {
   res.sendFile(path.join(root,'index.html'));
